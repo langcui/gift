8e108e827231fb8d93975a77e007a598e3f6d2af
langcui <langcui@yeah.net>
2020-08-07 14:04:25 +0800

    chore: test

--- test/effective_test.go
+++ test/effective_test.go
@@ -4,6 +4,9 @@ import "log"
 import "testing"
 import "io"
 import "os"
+import "time"
+import "fmt"
+import "runtime"
 
 func nextInt(pos int) (nextPos int) {
     nextPos = 10
@@ -42,3 +45,80 @@ func TestReadContents(t *testing.T) {
     log.Println(len(result), cap(result))
     return // 如果我们回到这里，f就关闭了。
 }
+
+
+func escape() *int {
+    i := 10
+    log.Printf("%v %T\n", i, i)
+    log.Printf("%v %T\n", &i, &i)
+
+    j := 10
+    log.Printf("j %v %T\n", j, j)
+    log.Printf("j %v %T\n", &j, &j)
+    return &i
+}
+
+func TestEscape(t *testing.T) {
+    a := escape()
+    log.Printf("%v %T\n", a, a)
+    log.Printf("%v %T\n", &a, &a)
+
+}
+
+func TestMap(t *testing.T) {
+    m := map[string]bool {
+        "a":true,
+        "b":true,
+    }
+
+    c, ok := m["c"]
+    log.Println(c)
+    log.Println(ok)
+}
+
+const (
+    a = iota
+    b
+    c = iota
+    d
+)
+
+func TestIota(t *testing.T) {
+    log.Printf("a:%v, b:%v, c:%v, d:%v.\n", a, b, c, d)
+}
+
+func TestChan(t *testing.T) {
+    arr := []int{1,2,3}
+    for i := range arr {
+        log.Println("i:", i)
+        j := i
+        go func (){log.Println("in go:", j)}()
+   //     time.Sleep(time.Second * 1)
+        log.Println("i:", i)
+    }
+
+    fmt.Println("cpu num:", runtime.NumCPU())
+    fmt.Println("cpu GOMAXPROCS:", runtime.GOMAXPROCS(1))
+    time.Sleep(time.Second * 2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
